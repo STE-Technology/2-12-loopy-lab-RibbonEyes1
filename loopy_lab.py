464fc293b91d9d24a6baa8d1a9da513e05bbde99
RibbonEyes1 <Joceline.marinhernandez25@ycdsbk1.ca>
2024-03-26 10:19:50 -0400

    Finished sector 5

--- loopy_lab.py
+++ loopy_lab.py
@@ -14,11 +14,13 @@ pygame.init()
 
 # Set up the display
 width, height = 1200, 600
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height)) 
 pygame.display.set_caption("Loopy Lab")
 
 # Define colours
 WHITE = (255, 255, 255)
+MAROON=(117, 15, 39)
+ORANGE=(240, 135, 70)
 BLACK = (0, 0, 0)
 
 # Main loop
@@ -32,12 +34,16 @@ while running:
     
     # Draw graphics
     # --(YOUR CODE STARTS HERE)-----------------------------------------------
-    screen.fill(WHITE)
+    screen.fill(MAROON)
 
     # Draw a 50x50 pixel black square with top-left corner at (x, y) = (5, 10)
     pygame.draw.rect(screen, BLACK, pygame.Rect(5, 10, 50, 50))
-
-
+    x_orange=300
+    for line in range(2,301,8):
+      for x_orange in range (300,601,8):
+        pygame.draw.rect(screen,ORANGE,pygame.Rect(line,x_orange,5,5))
+    
+     
 
 
 
