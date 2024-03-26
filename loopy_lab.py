458ec1f0f36368821019c908716ab8de296b989c
RibbonEyes1 <Joceline.marinhernandez25@ycdsbk1.ca>
2024-03-26 10:58:34 -0400

    NOt done! trying to figure out some loops

--- loopy_lab.py
+++ loopy_lab.py
@@ -13,7 +13,7 @@ import sys
 pygame.init()
 
 # Set up the display
-width, height = 1200, 600
+width, height = 800, 400
 screen = pygame.display.set_mode((width, height)) 
 pygame.display.set_caption("Loopy Lab")
 
@@ -22,7 +22,7 @@ WHITE = (255, 255, 255)
 MAROON=(117, 15, 39)
 ORANGE=(240, 135, 70)
 BLACK = (0, 0, 0)
-
+YELLOW=(245, 224, 118)
 # Main loop
 running = True
 
@@ -37,11 +37,34 @@ while running:
     screen.fill(MAROON)
 
     # Draw a 50x50 pixel black square with top-left corner at (x, y) = (5, 10)
-    pygame.draw.rect(screen, BLACK, pygame.Rect(5, 10, 50, 50))
+    #pygame.draw.rect(screen, BLACK, pygame.Rect(5, 10, 50, 50))
+
     x_orange=300
-    for line in range(2,301,8):
-      for x_orange in range (300,601,8):
+    for line in range(2,201,8):
+      y_yellow=201
+      for x_orange in range (1,y_yellow,8):
+        if y_yellow<10:
+            y_yellow=y_yellow-8
+            pygame.draw.rect(screen,YELLOW,pygame.Rect(line,x_orange,5,5))
+
+   
+   
+   
+   
+   
+    for line in range(2,201,8):
+      for x_orange in range (200,401,8):
+    
         pygame.draw.rect(screen,ORANGE,pygame.Rect(line,x_orange,5,5))
+
+    
+  
+    for line in range(202,402,8):
+      
+      for x_orange in range (200,401,8):
+        if line  :
+         WHITE=ORANGE
+         pygame.draw.rect(screen,WHITE,pygame.Rect(line,x_orange,5,5))
     
      
 
